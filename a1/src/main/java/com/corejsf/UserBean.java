abf9cd0525b54cfffb947cdfc4a6d191eda8589b
Steven Hsu <admin@stevenhsu.ca>
2013-10-09 12:18:07 -0700

    Add basic COMP1510 checkstyle.

--- a1/src/main/java/com/corejsf/UserBean.java
+++ a1/src/main/java/com/corejsf/UserBean.java
@@ -3,8 +3,14 @@ package com.corejsf;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
+/**
+ * User CDI Bean.
+ * 
+ * @author shsu
+ * @version 0.1
+ */
 @ApplicationScoped
 @Named("User")
-public class UserBean{
-    
+public class UserBean {
+
 }
