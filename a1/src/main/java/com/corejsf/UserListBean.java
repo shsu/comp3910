5bbe4e45c637cc9cd6cc6353ccf25159bc02d103
Steven Hsu <admin@stevenhsu.ca>
2013-10-12 00:31:46 -0700

    UserListBean initial commit.

---
+++ a1/src/main/java/com/corejsf/UserListBean.java
@@ -0,0 +1,18 @@
+package com.corejsf;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+/**
+ * UserList Bean holds list of regular and super users.
+ *
+ * @author shsu
+ * @version 0.1
+ */
+@ApplicationScoped
+@Named("UserList")
+public class UserListBean implements Serializable {
+
+}
