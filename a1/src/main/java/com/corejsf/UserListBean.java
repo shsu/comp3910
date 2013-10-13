5775c26e9d424f5b692ffe88b4fe18ba68866cd3
Steven Hsu <admin@stevenhsu.ca>
2013-10-12 20:33:20 -0700

    Add basic CRUD to userListBean.

--- a1/src/main/java/com/corejsf/UserListBean.java
+++ a1/src/main/java/com/corejsf/UserListBean.java
@@ -1,6 +1,8 @@
 package com.corejsf;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
@@ -14,5 +16,57 @@ import javax.inject.Named;
 @ApplicationScoped
 @Named("UserList")
 public class UserListBean implements Serializable {
+
+    /** The users. */
+    private List<UserBean> users = new ArrayList<UserBean>();
+
+    /**
+     * Gets the users. [READ]
+     *
+     * @return the users
+     */
+    public List<UserBean> getUsers() {
+        return users;
+    }
+
+    /**
+     * Sets the users. [UPDATE]
+     *
+     * @param users
+     *            the new users
+     */
+    public void setUsers(final List<UserBean> users) {
+        this.users = users;
+    }
+
+    /**
+     * Creates the user. [CREATE]
+     *
+     * @param employeeID
+     *            the employee id
+     * @param username
+     *            the username
+     * @param password
+     *            the password
+     * @return the string
+     */
+    public String createUser(final int employeeID, final String username,
+            final String password) {
+        users.add(new UserBean(employeeID, username, password, false));
+
+        return null;
+    }
+
+    /**
+     * Delete user. [DELETE]
+     *
+     * @param userToDelete
+     *            the user to delete
+     * @return the string
+     */
+    public String deleteUser(final UserBean userToDelete) {
+        users.remove(userToDelete);
+        return null;
+    }
 
 }
