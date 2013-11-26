ceaaa978fd2214cde56309b651b5cc089b0acbc7
Steven Hsu <admin@stevenhsu.ca>
2013-11-25 16:09:40 -0800

    Make marking done on the android client side.

--- a3/a3-server-jhou-shsu/src/main/java/ca/bcit/infosys/a3/server/services/QuizResource.java
+++ a3/a3-server-jhou-shsu/src/main/java/ca/bcit/infosys/a3/server/services/QuizResource.java
@@ -3,7 +3,6 @@ package ca.bcit.infosys.a3.server.services;
 import ca.bcit.infosys.a3.server.access.QuestionDao;
 import ca.bcit.infosys.a3.server.access.ResultDao;
 import ca.bcit.infosys.a3.server.domain.Question;
-import ca.bcit.infosys.a3.server.domain.Result;
 import ca.bcit.infosys.a3.server.logic.UserSession;
 
 import javax.ejb.EJB;
@@ -12,8 +11,6 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.io.Serializable;
-import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 @RequestScoped
@@ -32,53 +29,18 @@ public class QuizResource implements Serializable {
     @GET
     @Path("{week}")
     @Produces("application/json")
-    public List<Question> getQuizNoAnswers(@HeaderParam("token") final String token, @PathParam("week") int week) {
+    public List<Question> getQuizForWeek(@HeaderParam("token") final String token, @PathParam("week") int week) {
         if (!userSession.verifyToken(token)) {
             throw new WebApplicationException(Response.Status.UNAUTHORIZED);
         }
 
-        List<Question> questions = new ArrayList<Question>();
-
-        for (Question question : questionDao.getAllForWeek(week)) {
-            question.setAnswer(null);
-            questions.add(question);
-        }
+        List<Question> questions = questionDao.getAllForWeek(week);
 
         if (questions.isEmpty()) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
 
         return questions;
-    }
-
-    @PUT
-    @Path("{week}/mark")
-    @Consumes("application/json")
-    public Response markQuiz(@HeaderParam("token") final String token, @PathParam("week") int week, List<Character> results) {
-        if (!userSession.verifyToken(token)) {
-            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
-        }
-
-        if (results == null) {
-            throw new WebApplicationException(Response.Status.BAD_REQUEST);
-        }
-
-        List<Question> answerKey = questionDao.getAllForWeek(week);
-        short score = 0;
-
-        try {
-            for (short i = 0; i < results.size(); i++) {
-                if (results.get(i) == answerKey.get(i).getAnswer()) {
-                    score++;
-                }
-            }
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            throw new WebApplicationException(Response.Status.BAD_REQUEST);
-        }
-
-        resultDao.update(new Result(userSession.getUserID(), week, score, answerKey.size()));
-
-        return Response.created(URI.create("/results/" + week)).build();
     }
 
 }
